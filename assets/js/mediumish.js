76138c197f5cfdd3f39ffe80d6c86b1417daf312
Luiz Gabriel <luizgabriel515@gmail.com>
2023-05-06 18:19:00 -0300

    resolve console error

--- assets/js/mediumish.js
+++ assets/js/mediumish.js
@@ -114,13 +114,13 @@ jQuery(document).ready(function($){
  });   
 
 // deferred style loading
-var loadDeferredStyles = function () {
-	var addStylesNode = document.getElementById("deferred-styles");
-	var replacement = document.createElement("div");
-	replacement.innerHTML = addStylesNode.textContent;
-	document.body.appendChild(replacement);
-	addStylesNode.parentElement.removeChild(addStylesNode);
-};
+// var loadDeferredStyles = function () {
+// 	var addStylesNode = document.getElementById("deferred-styles");
+// 	var replacement = document.createElement("div");
+// 	replacement.innerHTML = addStylesNode.textContent;
+// 	document.body.appendChild(replacement);
+// 	addStylesNode.parentElement.removeChild(addStylesNode);
+// };
 var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
 	window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 if (raf) raf(function () {
